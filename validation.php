20775f84f977a89239aa00a36ceb3c8f1dabd371
Guillaume377 <guillaumestephan@live.fr>
2023-05-30 12:24:14 +0200

    structure de base

---
+++ validation.php
@@ -0,0 +1,29 @@
+<?php
+//inclure le fichier des fonctions pour pouvoir les appeler ici
+include 'functions.php';
+
+session_start();
+
+//inclure le head avec les balises de base + la balise head (pour ne pas répéter le code qu'il contient)
+include 'head.php';
+
+?>
+
+<body>
+    <?php
+    include 'header.php';
+    ?>
+
+    <main>
+        <?php
+        ?>
+    </main>
+
+    <?php
+    include 'footer.php';
+    ?>
+
+
+
+
+
